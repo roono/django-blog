910a9f4b2673c982c82fb6b54a92407209155ee1
roono <runoor18@gmail.com>
2023-02-18 23:28:42 -0800

    Added Categories

--- polling/urls.py
+++ polling/urls.py
@@ -4,4 +4,5 @@ from polling.views import list_view, detail_view
 urlpatterns = [
     path('', list_view, name="poll_index"),
     path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    
 ]
